0ac19a67a192c0598c9c79988bd3651cbe46ea8a
JnBanks <joku.banks@gmail.com>
2022-05-17 22:09:27 -0700

    add employee function added

--- index.js
+++ index.js
@@ -147,6 +147,75 @@ const addRole = () => {
   }
 
   // -- ADD EMPLOYEE
+  const addEmployee = () => {
+    db.query("SELECT * FROM employee", (err, eRes) => {
+      if (err) throw err;
+      const employeeChoice = [
+        {
+          name: 'None',
+          value: 0
+        }
+      ];
+      eRes.forEach(({ first_name, last_name, id }) => {
+        employeeChoice.push({
+          name: first_name + " " + last_name,
+          value: id
+        });
+      });
+      
+      db.query("SELECT * FROM role", (err, rolRes) => {
+        if (err) throw err;
+        const roleChoice = [];
+        rolRes.forEach(({ title, id }) => {
+          roleChoice.push({
+            name: title,
+            value: id
+            });
+          });
+       
+        let questions = [
+          {
+            type: "input",
+            name: "first_name",
+            message: "what is the employee's first name?"
+          },
+          {
+            type: "input",
+            name: "last_name",
+            message: "what is the employee's last name?"
+          },
+          {
+            type: "list",
+            name: "role_id",
+            choices: roleChoice,
+            message: "what is the employee's role?"
+          },
+          {
+            type: "list",
+            name: "manager_id",
+            choices: employeeChoice,
+            message: "who is the employee's manager? (could be null)"
+          }
+        ]
+    
+        inquirer.prompt(questions)
+          .then(response => {
+            const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)`;
+            let manager_id = response.manager_id !== 0? response.manager_id: null;
+            db.query(query, [[response.first_name, response.last_name, response.role_id, manager_id]], (err, res) => {
+              if (err) throw err;
+              console.log(`inserted employee ${response.first_name} ${response.last_name} with id ${res.insertId}`);
+              askQuestion();
+            });
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      })
+    });
+  }
+
+  // -- UPDATE ROLE
   
 
 askQuestion();
